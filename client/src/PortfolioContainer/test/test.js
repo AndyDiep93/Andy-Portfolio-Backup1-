a9d559009885a4ee38dbb6e2c86c60d352787dfe
Andy Diep <andy.diep@monogramsoftware.com>
2023-04-06 16:47:57 -0600

    new

--- client/src/PortfolioContainer/test/test.js
+++ client/src/PortfolioContainer/test/test.js
@@ -1,3 +1,10 @@
+import * as React from 'react';
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
 const React = require('react');
 const dayjs = require('dayjs');
 const xDatePickersInternalsDemo = require('@mui/x-date-pickers/internals/demo');
